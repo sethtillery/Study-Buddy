0c3dfb75cedc981741ced3ebaa1a7270d1f73842
mfwhite1 <104807584+mfwhite1@users.noreply.github.com>
2022-12-01 16:33:35 -0500

    WORKING FINAL TIMER

--- Study Buddy/Assets/NewTimer.cs
+++ Study Buddy/Assets/NewTimer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer: MonoBehaviour
+public class NewTimer: MonoBehaviour
 {
   public float timeValue = 90;
   public Text timerText;
